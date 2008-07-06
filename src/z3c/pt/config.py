5fa618ed64a8d3204beddee3971d5667d7b54aa5
Hanno Schlichting <hanno@hannosch.eu>
2008-07-06 09:57:39 +0000

    Added new `Z3C_PT_FILECACHE` environment variable pointing to a directory. If set, this will be used to cache the compiled files.

--- src/z3c/pt/config.py
+++ src/z3c/pt/config.py
@@ -1,6 +1,11 @@
 import os
+from os.path import abspath
 
 DEBUG_MODE = os.environ.get('Z3C_PT_DEBUG', 'false')
 DEBUG_MODE = DEBUG_MODE.lower() in ('yes', 'true', 'on')
 
 PROD_MODE = not DEBUG_MODE
+
+FILECACHE = os.environ.get('Z3C_PT_FILECACHE', None)
+if FILECACHE is not None:
+    FILECACHE = abspath(FILECACHE)
