d510a9424f8f179cf058165d2a380212734961cd
Stefan Eletzhofer <stefan.eletzhofer@inquant.de>
2008-12-02 13:42:37 +0000

    Actually commit the source, dumbo.

---
+++ src/z3c/pt/language.py
@@ -0,0 +1,51 @@
+from chameleon.core import translation
+from chameleon.core import config
+from chameleon.core import utils
+
+import chameleon.zpt.language
+
+
+class ZopePageTemplateElement(chameleon.zpt.language.ZopePageTemplateElement):
+    """Zope Page Template element.
+
+    Implements the ZPT subset of the attribute template language.
+    """
+    meta_interpolation = utils.attribute(
+        utils.meta_attr('interpolation'), default="false")
+
+class XHTMLElement(chameleon.zpt.language.XHTMLElement):
+    """XHTML namespace element."""
+    meta_interpolation = utils.attribute(
+        utils.meta_attr('interpolation'), default="false")
+
+class TALElement(chameleon.zpt.language.TALElement):
+    """TAL namespace element."""
+    meta_interpolation = utils.attribute(
+        utils.meta_attr('interpolation'), default="false")
+
+class METALElement(chameleon.zpt.language.METALElement):
+    """METAL namespace element."""
+    meta_interpolation = utils.attribute(
+        utils.meta_attr('interpolation'), default="false")
+
+class MetaElement(chameleon.zpt.language.MetaElement):
+    meta_interpolation = utils.attribute(
+        utils.meta_attr('interpolation'), default="false")
+
+class Parser(chameleon.zpt.language.Parser):
+    """Zope Page Template parser."""
+
+    element_mapping = {
+        config.XHTML_NS: {None: XHTMLElement},
+        config.META_NS: {None: MetaElement},
+        config.TAL_NS: {None: TALElement},
+        config.METAL_NS: {None: METALElement}}
+
+    fallback = XHTMLElement
+
+    default_expression = 'path'
+
+    def parse(self, body):
+        root, doctype = super(Parser, self).parse(body)
+        root.meta_translator = self.default_expression
+        return root, doctype
