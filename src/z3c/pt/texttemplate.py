fb3cb1df4c13cdd78e5eebe571ea9ffeb3f39515
Malthe Borch <mborch@gmail.com>
2009-02-25 16:19:04 +0000

    Expressions in text templates are never escaped.

--- src/z3c/pt/texttemplate.py
+++ src/z3c/pt/texttemplate.py
@@ -1,23 +1,34 @@
-import chameleon.zpt.language
+from chameleon.core import config
 
+import chameleon.zpt.language
 import pagetemplate
 
+class XHTMLElement(chameleon.zpt.language.XHTMLElement):
+    meta_interpolation_escaping = False
+
+class Parser(chameleon.zpt.language.Parser):
+    default_expression = "path"
+
+    element_mapping = {
+        config.XHTML_NS: {None: XHTMLElement},
+        }
+    
 class TextTemplate(pagetemplate.PageTemplate):
     __doc__ = pagetemplate.PageTemplate.__doc__ # for Sphinx autodoc
-    default_parser = chameleon.zpt.language.Parser(default_expression="path")
+    default_parser = Parser()
     format = 'text'
 
 class TextTemplateFile(pagetemplate.PageTemplateFile):
     __doc__ = pagetemplate.PageTemplateFile.__doc__ # for Sphinx autodoc
-    default_parser = chameleon.zpt.language.Parser(default_expression="path")
+    default_parser = Parser()
     format = 'text'
 
 class ViewTextTemplate(pagetemplate.ViewPageTemplate):
     __doc__ = pagetemplate.ViewPageTemplate.__doc__ # for Sphinx autodoc
-    default_parser = chameleon.zpt.language.Parser(default_expression="path")
+    default_parser = Parser()
     format = 'text'
 
 class ViewTextTemplateFile(pagetemplate.ViewPageTemplateFile):
     __doc__ = pagetemplate.ViewPageTemplateFile.__doc__ # for Sphinx autodoc
-    default_parser = chameleon.zpt.language.Parser(default_expression="path")
+    default_parser = Parser()
     format = 'text'
