f1b91e8aff8f227a8b35e45f8896f84e5d957964
Malthe Borch <mborch@gmail.com>
2009-05-20 07:11:56 +0000

    Preparing release.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 import sys
-version = '1.0b15'
+version = '1.0b16'
 
 install_requires = [
     'setuptools',
@@ -9,7 +9,7 @@ install_requires = [
     'zope.i18n >= 3.5',
     'zope.traversing',
     'zope.contentprovider',
-    'chameleon.zpt >= 1.0b16',
+    'chameleon.zpt >= 1.0b17',
     ]
 
 if sys.version_info[:3] < (2,5,0):
